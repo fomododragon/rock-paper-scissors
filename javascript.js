e19248adaa994328e0af8bf2d971de55d5b0af5e
kodomodragon <tnjnghng2@gmail.com>
2022-02-16 15:34:47 +0800

    request user input and generate one play round

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
-let computerShape = [
-    'Rock',
-    'Paper',
-    'Scissors'
+let shape = [
+    "rock",
+    "paper",
+    "scissors"
 ];
 
 function getRandomInt(max) {
@@ -10,9 +10,48 @@ function getRandomInt(max) {
 
 function computerPlay() {
     randomIndex = getRandomInt(3);
-    randomShape = computerShape[randomIndex];
+    computerSelection = shape[randomIndex];
+}
+
+function playerPlay() {
+    playerSelection = prompt("Let's Play: Rock, Paper or Scissors?").toLowerCase();
+
+    while (true) {
+        if (shape.includes(playerSelection)) {
+            break;
+        } playerSelection = prompt("Try Again: Rock, Paper or Scissors?").toLowerCase();
+    }
 }
 
 computerPlay();
+playerPlay();
 
-console.log(randomShape);
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+            return "It's a draw!";
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You Lose! Paper beats Rock"; 
+        }
+        if (computerSelection === "scissors") {
+            return "You Win! Rock beats Scissors"; 
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return  "You Lose! Scissors beats Paper"; 
+        }
+        if (computerSelection === "rock") {
+            return  "You Win! Paper beats Rock"; 
+        }
+    } else {
+        if (computerSelection === "rock") {
+            return  "You Lose! Rock beats Scissors"; 
+        }
+        if (computerSelection === "paper") {
+            return  "You Win! Scissors beats Paper"; 
+        }
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
